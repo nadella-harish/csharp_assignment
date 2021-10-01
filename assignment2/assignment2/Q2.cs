be1bb29aeb575d5a5d06f439dd89c68291f2f292
harish <hellookya@gmail.com>
2021-10-01 09:58:41 +0530

    all files are added

--- assignment2/assignment2/Q2.cs
+++ assignment2/assignment2/Q2.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace assignment2
 {
-    class Q2
-    {
-    }
+	class BankAccount
+	{
+		double Balance;
+
+		public BankAccount()
+		{
+			this.Balance = 500;
+		}
+		public BankAccount(double Balance)
+		{
+			this.Balance = Balance;
+		}
+
+		public double getbalance()
+		{
+			return Balance;
+		}
+	}
+
+	class finalbankaccount
+	{
+		public static void Main()
+		{
+
+			BankAccount b1 = new BankAccount();
+			BankAccount b2 = new BankAccount(50000);
+
+			Console.WriteLine("default balance = " + b1.getbalance());
+			Console.WriteLine("account balance = " + b2.getbalance());
+		}
+
+	}
 }
+
